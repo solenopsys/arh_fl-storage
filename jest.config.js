76691f5231788e3503e0612a24528b94553e54bb
AlexStorm <info@alexstorm.ru>
2023-02-26 09:18:29 +0530

    name refactoring

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../../coverage/packages/libs/storage',
+  coverageDirectory: '../../../coverage/packages/libraries/fl-storage',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
